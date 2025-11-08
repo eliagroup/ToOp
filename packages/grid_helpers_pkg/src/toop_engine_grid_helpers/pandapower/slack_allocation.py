6dd7014fe366c36c3b62dd16e82b3de98767abdd
siarhei <ser.matyushenok@gmail.com>
2025-11-08 15:35:43 +0100

    refactor: fix ruff issues

--- packages/grid_helpers_pkg/src/toop_engine_grid_helpers/pandapower/slack_allocation.py
+++ packages/grid_helpers_pkg/src/toop_engine_grid_helpers/pandapower/slack_allocation.py
@@ -1,33 +1,35 @@
-"""
-Utility functions for selecting and assigning slack generators, replacing sgens with gens, and analyzing generation-capable islands in pandapower networks.
-"""
+"""Utility functions for selecting and assigning slack generators."""
 
-from typing import Optional
+from typing import Optional, Union
 
 import networkx as nx
 import numpy as np
 import pandapower as pp
 import pandas as pd
 from pandapower.create import create_gen
-from pandapower.toolbox.grid_modification import _replace_group_member_element_type, \
-    _adapt_result_tables_in_replace_functions, _adapt_profiles_in_replace_functions
+from pandapower.toolbox.grid_modification import (
+    _adapt_profiles_in_replace_functions,
+    _adapt_result_tables_in_replace_functions,
+    _replace_group_member_element_type,
+)
 from toop_engine_grid_helpers.pandapower.network_topology_utils import collect_element_edges
 
 
 def _slack_allocation_tie_break(df_min: pd.DataFrame) -> tuple[int, str]:
     """
-        Returns the index of the selected (s)gen & the respective element type after applying tie break rules.
+    Return the index of the selected (s)gen & the respective element type after applying tie break rules.
 
-        Parameters
-        ----------
-        df_min : pd.DataFrame
+    Parameters
+    ----------
+    df_min : pd.DataFrame
+        A filtered DataFrame containing candidate generators/sgen entries
 
-        Returns
-        -------
-        tuple[int, str]
-            A tuple containing:
-            - The index of the selected element.
-            - The element type: either `"gen"` or `"sgen"`.
+    Returns
+    -------
+    tuple[int, str]
+        A tuple containing:
+        - The index of the selected element.
+        - The element type: either `"gen"` or `"sgen"`.
     """
     # fast path: no tie
     if len(df_min) == 1:
@@ -45,6 +47,64 @@ def _slack_allocation_tie_break(df_min: pd.DataFrame) -> tuple[int, str]:
     return int(tied_df.index[0]), str(tied_df["etype"].iloc[0])
 
 
+def _get_vm_pu_for_bus(net: pp.pandapowerNet, bus: np.int64) -> float:
+    """
+    Determine the vm_pu setpoint for a given bus.
+
+    Parameters
+    ----------
+    net : pandapowerNet
+        The pandapower network.
+    bus : int
+        The bus index whose vm_pu value should be retrieved.
+
+    Returns
+    -------
+    float
+        The vm_pu setpoint. Taken from res_bus if present, otherwise from
+        gen or ext_grid entries on the same bus. Falls back to 1.0 pu.
+    """
+    # res_bus has priority
+    if bus in net.res_bus.index:
+        return float(net.res_bus.at[bus, "vm_pu"])
+
+    # generator-defined setpoint
+    if bus in net.gen.bus.values:
+        return float(net.gen.vm_pu.loc[net.gen.bus == bus].values[0])
+
+    # external grid setpoint
+    if bus in net.ext_grid.bus.values:
+        return float(net.ext_grid.vm_pu.loc[net.ext_grid.bus == bus].values[0])
+
+    # fallback
+    return 1.0
+
+
+def _handle_replaced_sgen(net: pp.pandapowerNet, sgen: int, retain_sgen_elm: bool) -> None:
+    """
+    Handle removal or deactivation of an sgen after replacement.
+
+    Parameters
+    ----------
+    net : pp.pandapowerNet
+        The pandapower network.
+    sgen : int
+        Index of the sgen element being replaced.
+    retain_sgen_elm : bool
+        Whether to keep the sgen element but mark it out of service.
+    """
+    if retain_sgen_elm:
+        net.sgen.loc[sgen, "in_service"] = False
+
+        # create column only if missing
+        if "replaced_by_gen" not in net.sgen.columns:
+            net.sgen["replaced_by_gen"] = False
+
+        net.sgen.at[sgen, "replaced_by_gen"] = True
+    else:
+        net.sgen = net.sgen.drop(sgen)
+
+
 def replace_sgen_by_gen(
         net: pp.pandapowerNet,
         sgen: int,
@@ -52,31 +112,32 @@ def replace_sgen_by_gen(
         retain_sgen_elm: bool = True,
 ) -> int:
     """
-        Replaces an sgen with a gen element. A new column "replaced_sgen" is created in net.gen which is
-        set to True only for the new gen element. net.gen.vm_pu is set based on power flow results, if
-        available, otherwise the default value of 1.0 pu is used.
+    Replace an sgen with a gen element.
 
-        Parameters
-        ----------
-        net : pp.pandapowerNet
-            The pandapower network object.
-        sgen : int
-            sgen index to be replaced by gen
-        cols_to_keep : Optional[list[str]]
-            List of column names which should be kept while replacing sgen. If None, these columns
-            are kept if values exist: "max_p_mw", "min_p_mw", "max_q_mvar", "min_q_mvar". However,
-            these columns are always set: "bus", "vm_pu", "p_mw", "name", "in_service", "controllable".
-        retain_sgen_elm : bool
-            Flag to indicate whether to retain or drop the sgen element in net.sgen. If True, this element
-            is set as out of service (in_service=False) and a new column "replaced_by_gen" is added which
-            is set to True only for this element.
+    A new column "replaced_sgen" is created in net.gen which is
+    set to True only for the new gen element. net.gen.vm_pu is set based on power flow results, if
+    available, otherwise the default value of 1.0 pu is used.
 
-        Returns
-        -------
-        new_idx : int
-            The new gen index.
+    Parameters
+    ----------
+    net : pp.pandapowerNet
+        The pandapower network object.
+    sgen : int
+        sgen index to be replaced by gen
+    cols_to_keep : Optional[list[str]]
+        List of column names which should be kept while replacing sgen. If None, these columns
+        are kept if values exist: "max_p_mw", "min_p_mw", "max_q_mvar", "min_q_mvar". However,
+        these columns are always set: "bus", "vm_pu", "p_mw", "name", "in_service", "controllable".
+    retain_sgen_elm : bool
+        Flag to indicate whether to retain or drop the sgen element in net.sgen. If True, this element
+        is set as out of service (in_service=False) and a new column "replaced_by_gen" is added which
+        is set to True only for this element.
+
+    Returns
+    -------
+    new_idx : int
+        The new gen index.
     """
-
     # --- error handling
     if not isinstance(sgen, (int, np.integer)):
         raise ValueError("sgen must be a positive integer")
@@ -106,17 +167,7 @@ def replace_sgen_by_gen(
     sgen_row = net.sgen.loc[sgen]
     bus = sgen_row.bus
 
-    # assign vm_pu set point
-    if bus in net.res_bus.index:
-        vm_pu = net.res_bus.at[bus, "vm_pu"]
-    else:  # no result information to get vm_pu
-        # use net.gen.vm_pu or net.ext_grid.vm_pu or set 1.0 pu
-        if bus in net.gen.bus.values:
-            vm_pu = net.gen.vm_pu.loc[net.gen.bus == bus].values[0]
-        elif bus in net.ext_grid.bus.values:
-            vm_pu = net.ext_grid.vm_pu.loc[net.ext_grid.bus == bus].values[0]
-        else:
-            vm_pu = 1.0
+    vm_pu = _get_vm_pu_for_bus(net, bus)
 
     controllable = False if "controllable" not in net.sgen.columns else sgen_row.controllable
 
@@ -146,14 +197,7 @@ def replace_sgen_by_gen(
     _adapt_profiles_in_replace_functions(net, "sgen", [sgen], "gen", [new_idx])
 
     # --- drop replaced sgens
-    if retain_sgen_elm is True:
-        net.sgen.loc[sgen, "in_service"] = False
-        # populate replaced_by_gen sgen column
-        if "replaced_by_gen" not in net.sgen.columns:
-            net.sgen["replaced_by_gen"] = False
-        net.sgen.at[sgen, "replaced_by_gen"] = True
-    else:
-        net.sgen = net.sgen.drop(sgen)
+    _handle_replaced_sgen(net, sgen, retain_sgen_elm)
 
     # --- adapt cost data
     for table in ["pwl_cost", "poly_cost"]:
@@ -169,23 +213,24 @@ def replace_sgen_by_gen(
 
 def get_generating_units_with_load(net: pp.pandapowerNet) -> set[int]:
     """
-        Return all bus indices that host generation *and/or* load elements.
+    Return all bus indices that host generation *and/or* load elements.
 
-        Includes:
-            - gen
-            - sgen
-            - ext_grid
-            - ward / xward
-            - load
+    Includes:
+        - gen
+        - sgen
+        - ext_grid
+        - ward / xward
+        - load
 
-        Parameters
-        ----------
-        net : pp.pandapowerNet
-            The pandapower network object.
-        Returns
-        -------
-        set[int]
-            Unique bus indices that have either generation or load.
+    Parameters
+    ----------
+    net : pp.pandapowerNet
+        The pandapower network object.
+
+    Returns
+    -------
+    set[int]
+        Unique bus indices that have either generation or load.
     """
 
     # Safely handle cases where some tables may be empty or missing
@@ -207,18 +252,17 @@ def get_generating_units_with_load(net: pp.pandapowerNet) -> set[int]:
 
 def get_buses_with_reference_sources(net: pp.pandapowerNet) -> set[int]:
     """
-        Return all bus indices that have generators or static generators
-        with a positive referencePriority value.
+    Return all bus indices that have generators or static generators with a positive referencePriority value.
 
-        Parameters
-        ----------
-        net : pp.pandapowerNet
-            The pandapower network object.
+    Parameters
+    ----------
+    net : pp.pandapowerNet
+        The pandapower network object.
 
-        Returns
-        -------
-        set[int]
-            Set of bus indices with reference-capable generators or static generators.
+    Returns
+    -------
+    set[int]
+        Set of bus indices with reference-capable generators or static generators.
     """
     gen_buses = set(net.gen.loc[net.gen["referencePriority"].fillna(0) > 0, "bus"].astype(int))
     sgen_buses = set(net.sgen.loc[net.sgen["referencePriority"].fillna(0) > 0, "bus"].astype(int))
@@ -227,33 +271,33 @@ def get_buses_with_reference_sources(net: pp.pandapowerNet) -> set[int]:
 
 def assign_slack_gen_by_weight(net: pp.pandapowerNet, bus_idx_set: set[np.int64]) -> tuple[int, str]:
     """
-        Selects the (s)gen index to be assigned as slack based on referencePriority rules.
+    Select the (s)gen index to be assigned as slack based on referencePriority rules.
 
-        Logic:
-          1. Filter (s)gens by bus∈bus_idx_set and non-NaN/positive referencePriority.
-             If the network is to be reduced to 50Hz area, (s)gens located on the Danish area
-             (their bus zone containing "EnDK" string) are excluded to ensure correct reduction.
-          2. Find minimum referencePriority among those.
-          3. If exactly one gen has that min weight, choose it.
-             Otherwise, among the tied:
-               - If any sn_mva is non-NaN, pick those with the max sn_mva.
-               - Else (all NaN), tie-break by random choice.
-          4. If an sgen element is selected, further processing is required to convert sgen to gen
-             and set slack=True.
+    Logic:
+      1. Filter (s)gens by bus∈bus_idx_set and non-NaN/positive referencePriority.
+         If the network is to be reduced to 50Hz area, (s)gens located on the Danish area
+         (their bus zone containing "EnDK" string) are excluded to ensure correct reduction.
+      2. Find minimum referencePriority among those.
+      3. If exactly one gen has that min weight, choose it.
+         Otherwise, among the tied:
+           - If any sn_mva is non-NaN, pick those with the max sn_mva.
+           - Else (all NaN), tie-break by random choice.
+      4. If an sgen element is selected, further processing is required to convert sgen to gen
+         and set slack=True.
 
-        Parameters
-        ----------
-        net : pp.pandapowerNet
-            The pandapower network object.
-        bus_idx_set : set[int]
-            Set of bus indices to consider (e.g., a connected component).
+    Parameters
+    ----------
+    net : pp.pandapowerNet
+        The pandapower network object.
+    bus_idx_set : set[int]
+        Set of bus indices to consider (e.g., a connected component).
 
-        Returns
-        -------
-        tuple[int, str]
-            A tuple containing:
-            - The index of the chosen element (in `net.gen` or `net.sgen`).
-            - The element type: either `"gen"` or `"sgen"`.
+    Returns
+    -------
+    tuple[int, str]
+        A tuple containing:
+        - The index of the chosen element (in `net.gen` or `net.sgen`).
+        - The element type: either `"gen"` or `"sgen"`.
     """
     # Filter gens/sgens by bus set and positive referencePriority
     mask_gen = (
@@ -290,39 +334,39 @@ def assign_slack_gen_by_weight(net: pp.pandapowerNet, bus_idx_set: set[np.int64]
 
 def assign_slack_per_island(
         net: pp.pandapowerNet,
-        net_graph,
+        net_graph: Union[nx.Graph, nx.MultiGraph],
         bus_lookup: list[int],
         elements_ids: list[str],
         min_island_size: int
 ) -> list[tuple[int, int]]:
     """
-        Assigns one slack generator per valid island in the network after isolating specific elements.
+    Assign one slack generator per valid island in the network after isolating specific elements.
 
-        This function deactivates all existing slack generators, removes selected element edges
-        to simulate outages or disconnections, and identifies independent network islands.
-        For each valid island (meeting size and generation criteria), it assigns a new slack generator
-        based on generator weighting rules.
+    This function deactivates all existing slack generators, removes selected element edges
+    to simulate outages or disconnections, and identifies independent network islands.
+    For each valid island (meeting size and generation criteria), it assigns a new slack generator
+    based on generator weighting rules.
 
-        Parameters
-        ----------
-        net : pandapowerNet
-            The pandapower network object containing buses, lines, generators, etc.
-        net_graph : networkx.Graph
-            The network graph representation of the power system.
-        bus_lookup : list[int]
-            A list mapping graph node indices to pandapower bus indices.
-        elements_ids : list[str]
-            A list of element IDs (e.g., line or transformer IDs) to deactivate or remove.
-        min_island_size : int
-            Minimum number of nodes required for an island to be considered valid.
+    Parameters
+    ----------
+    net : pandapowerNet
+        The pandapower network object containing buses, lines, generators, etc.
+    net_graph : Union[nx.Graph, nx.MultiGraph]
+        The network graph representation of the power system.
+    bus_lookup : list[int]
+        A list mapping graph node indices to pandapower bus indices.
+    elements_ids : list[str]
+        A list of element IDs (e.g., line or transformer IDs) to deactivate or remove.
+    min_island_size : int
+        Minimum number of nodes required for an island to be considered valid.
 
-        Returns
-        -------
-        list of tuple[int, int]
-            List of edges (tuples of node indices) that were removed from the graph.
+    Returns
+    -------
+    list of tuple[int, int]
+        List of edges (tuples of node indices) that were removed from the graph.
     """
-    if ((not net.sgen.empty and not "referencePriority" in net.sgen.columns)
-            or (not net.gen.empty and not "referencePriority" in net.gen.columns)):
+    if ((not net.sgen.empty and "referencePriority" not in net.sgen.columns)
+            or (not net.gen.empty and "referencePriority" not in net.gen.columns)):
         # This function requires 'referencePriority' columns in both sgen and gen tables.
         # Networks without these columns are not supported.
         return []

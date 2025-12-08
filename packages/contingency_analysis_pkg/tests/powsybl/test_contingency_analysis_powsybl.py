95fbfb6a74dbad41acecf3998240a60aa59572d5
Bpetrick <benjamin.petrick@externel.be>
2025-12-08 15:34:47 +0100

    fix(contingency_analysis): fix run_contingency_analysis_powsybl() when the basecase does not converge (#37)
    * fix(contingency_analysis): fix run_contingency_analysis_powsybl() when the basecase does not converge
    
    * test: fix name of test test_run_contingency_analysis_powsybl_not_converging_basecase()
    
    * test(dc_solver): increase timeout for performance test, due to less compute in ci
    
    Signed-off-by: Benjamin Petrick <benjamin.petrick@externel.be>

--- packages/contingency_analysis_pkg/tests/powsybl/test_contingency_analysis_powsybl.py
+++ packages/contingency_analysis_pkg/tests/powsybl/test_contingency_analysis_powsybl.py
@@ -6,6 +6,8 @@ from toop_engine_contingency_analysis.pypowsybl import (
     run_powsybl_analysis,
     translate_nminus1_for_powsybl,
 )
+from toop_engine_contingency_analysis.pypowsybl.contingency_analysis_powsybl import run_contingency_analysis_powsybl
+from toop_engine_interfaces.nminus1_definition import Contingency, GridElement, Nminus1Definition
 
 
 def test_run_powsybl_analysis(powsybl_bus_breaker_net: pypowsybl.network.Network) -> None:
@@ -72,3 +74,26 @@ def test_contingency_analysis_ray_vs_powsybl(powsybl_net: str, request):
         n_processes=2,
     )
     assert lf_parallel_ray_polars == lf_parallel_native_polars
+
+
+def test_run_contingency_analysis_powsybl_not_converging_basecase() -> None:
+    net = pypowsybl.network.create_ieee14()
+    loads = net.get_loads(attributes=["q0"])
+    # make the ac loadflow fail
+    loads["q0"] = loads["q0"] * 10
+    net.update_loads(loads)
+
+    nminus1_def_1 = Nminus1Definition(
+        monitored_elements=[
+            GridElement(id=index, name=row.name, kind="branch", type=row.type)
+            for index, row in net.get_branches().iterrows()
+        ],
+        contingencies=[Contingency(id="BASECASE", elements=[])],
+    )
+
+    result = run_contingency_analysis_powsybl(
+        net=net, n_minus_1_definition=nminus1_def_1, job_id="same_test_job", timestep=0, method="ac", polars=True
+    )
+    assert len(result.converged.collect()) == 1
+    assert result.converged.collect()["contingency"][0] == "BASECASE"
+    assert result.converged.collect()["status"][0] == "MAX_ITERATION_REACHED"

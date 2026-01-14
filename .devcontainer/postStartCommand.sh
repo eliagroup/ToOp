5535aed78cc793d87ee5d1b91fe841bf45c318c4
LeonHilf <149764770+LeonHilf@users.noreply.github.com>
2026-01-14 10:32:04 +0000

    chore: Add more license headers

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Copyright 2025 50Hertz Transmission GmbH and Elia Transmission Belgium
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+# If a copy of the MPL was not distributed with this file,
+# you can obtain one at https://mozilla.org/MPL/2.0/.
+# Mozilla Public License, version 2.0
+
 # Configure poetry with access token
 # no poetry anymore + uv has no such config
 # poetry config http-basic.MCCS $MCCS_USER $MCCS_MODULE_PULL_KEY

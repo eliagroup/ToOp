1597e30fcfa3576fe501cdeeb84691c968d0a5b5
nicow-elia <129943275+nicow-elia@users.noreply.github.com>
2026-02-05 18:42:30 +0100

    chore: Rename copyright (#110)

--- packages/interfaces_pkg/tests/messages/preprocess/test_preprocess_heartbeat.py
+++ packages/interfaces_pkg/tests/messages/preprocess/test_preprocess_heartbeat.py
@@ -1,4 +1,4 @@
-# Copyright 2026 50Hertz Transmission GmbH and Elia Transmission Belgium
+# Copyright 2026 50Hertz Transmission GmbH and Elia Transmission Belgium SA/NV
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 # If a copy of the MPL was not distributed with this file,

a95efba6ca3f2fb08d8e0c6121c59ecf45f54bae
Leonard Hilfrich <Leonard.Hilfrich@50Hertz.com>
2026-03-04 13:53:35 +0100

    test: improve tests after copilot feedback
    Signed-off-by: Leonard Hilfrich <Leonard.Hilfrich@50Hertz.com>

--- packages/topology_optimizer_pkg/tests/test_jax_compilation.py
+++ packages/topology_optimizer_pkg/tests/test_jax_compilation.py
@@ -31,13 +31,17 @@ def test_jax_no_tracing_flag() -> None:
     def change_type(x: Int[Array, " len_array"]) -> Float[Array, " len_array"]:
         return x.astype(jnp.float32)
 
+    def increment_by_more(x: Int[Array, " len_array"]) -> Shaped[Array, " len_array"]:
+        return x + 2
+
     increment_jit = jax.jit(increment)
     change_type_jit = jax.jit(change_type)
+    increment_by_more_jit = jax.jit(increment_by_more)
 
     result = jnp.array(0, dtype=jnp.int32)
-    other_values_result = jnp.array([1], dtype=jnp.int32)
+    other_values_result = jnp.array(1, dtype=jnp.int32)
+    other_shape_result = jnp.array([1], dtype=jnp.int32)
     other_dtypes_result = jnp.array([0], dtype=jnp.float32)
-    other_shapes_result = jnp.array([0, 1], dtype=jnp.int32)
     # Compile once for the traced shape/dtype, and once for each non-traced argument variation.
     new_traced_result = increment_jit(result)
 
@@ -46,28 +50,29 @@ def test_jax_no_tracing_flag() -> None:
     increment_jit(new_traced_result)
     increment_jit(other_dtypes_result)
     increment_jit(other_values_result)
-    increment_jit(other_shapes_result)
+    increment_jit(other_shape_result)
 
     jax.clear_caches()
     new_traced_result = increment_jit(result)
     changed_type_trace_result = change_type_jit(result)
+    more_increment_trace_result = increment_by_more_jit(result)
     with jax.no_tracing():
         increment_jit(result)  # Should not re-trace or raise.
         increment_jit(new_traced_result)  # Should not re-trace or raise.
+        increment_jit(other_values_result)  # Should not re-trace or raise.
+        increment_by_more_jit(more_increment_trace_result)  # Should not re-trace or raise.
         with pytest.raises(RuntimeError, match="re-tracing function increment"):
             increment_jit(changed_type_trace_result)  # Traced array from other function with different dtype should raise.
         with pytest.raises(RuntimeError, match="re-tracing function increment"):
-            increment_jit(other_dtypes_result)  # Non-traced array with other values should raise
+            increment_jit(other_dtypes_result)  # Non-traced array with other dtype should raise
         with pytest.raises(RuntimeError, match="re-tracing function increment"):
-            increment_jit(other_values_result)  # Non-traced array with different dtype  should raise
-        with pytest.raises(RuntimeError, match="re-tracing function increment"):
-            increment_jit(other_shapes_result)
+            increment_jit(other_shape_result)
 
 
-def _initialize_small_optimizer(static_information_file: str) -> tuple[DiscreteMapElites, JaxOptimizerData]:
+def _initialize_small_optimizer(static_information_file: str, batch_size: int) -> tuple[DiscreteMapElites, JaxOptimizerData]:
     static_information = load_static_information(static_information_file)
     algo, jax_data = initialize_genetic_algorithm(
-        batch_size=2,
+        batch_size=batch_size,
         max_num_splits=2,
         max_num_disconnections=2,
         static_informations=(static_information,),
@@ -96,7 +101,7 @@ def test_dc_optimization_does_not_retrace(
     no re-compilations occur
     """
     jax.clear_caches()
-    algo, jax_data = _initialize_small_optimizer(static_information_file)
+    algo, jax_data = _initialize_small_optimizer(static_information_file, batch_size=2)
 
     with jax.no_tracing():
         with pytest.raises(RuntimeError, match="re-tracing function"):
@@ -117,9 +122,9 @@ def test_dc_optimization_does_not_retrace(
             run_single_device_epoch(jax_data_traced_2, 2, algo.update)  # Should raise due to non-traced input.
         with pytest.raises(RuntimeError, match="re-tracing function"):
             # untraced input of jax data leads to recompilation
-            _, jax_data_new = _initialize_small_optimizer(static_information_file)
+            _, jax_data_increased_batch = _initialize_small_optimizer(static_information_file, batch_size=4)
 
-            run_single_device_epoch(jax_data_new, 1, algo.update)  # Should raise due to non-traced input.
+            run_single_device_epoch(jax_data_increased_batch, 1, algo.update)  # Should raise due to non-traced input.
     # Check sensibility of the results
     # This leads to retracing, so outside of the no_tracing context
     # First run

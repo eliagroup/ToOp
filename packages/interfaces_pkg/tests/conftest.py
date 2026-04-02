4219117fa577f412395990cfd142edeb64e6e300
Miha Sajko <miha.sajko@elia.be>
2026-04-02 17:01:29 +0200

    reset structlog
    Signed-off-by: Miha Sajko <miha.sajko@elia.be>

---
+++ packages/interfaces_pkg/tests/conftest.py
@@ -0,0 +1,17 @@
+# Copyright 2026 50Hertz Transmission GmbH and Elia Transmission Belgium SA/NV
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+# If a copy of the MPL was not distributed with this file,
+# you can obtain one at https://mozilla.org/MPL/2.0/.
+# Mozilla Public License, version 2.0
+
+"""Pytest fixtures."""
+
+import pytest
+import structlog
+
+
+@pytest.fixture(autouse=True)
+def reset_structlog() -> None:
+    """Reset structlog between tests."""
+    structlog.reset_defaults()

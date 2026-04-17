5e2a7a8e9b1369725c6f51da7161bf03f0406f6e
LeonHilf <149764770+LeonHilf@users.noreply.github.com>
2026-04-17 10:49:55 +0200

    feat: enable hybrid grid support (#367)
    ## Checklist
    
    Please check if the PR fulfills these requirements:
    
    - [ ] PR Title follows conventional commit messages
    - [ ] Tests for the changes have been added (for bug fixes / features)
    - [ ] Docs have been added / updated (for bug fixes / features)
    - [ ] All commits in this PR are DCO signed-off (see CONTRIBUTING.md)
    
    ## Does this PR already have an issue describing the problem?
    
    Fixes #
    
    ## What is the new behavior (if this is a feature change)?
    
    <!-- Describe the new behavior and the motivation/context for the
    change. -->
    
    ## Does this PR introduce a breaking change?
    
    - [ ] Yes
    - [ ] No
    
    ---------
    
    Signed-off-by: Leonard Hilfrich <Leonard.Hilfrich@50Hertz.com>

--- packages/interfaces_pkg/src/toop_engine_interfaces/messages/preprocess/preprocess_commands.py
+++ packages/interfaces_pkg/src/toop_engine_interfaces/messages/preprocess/preprocess_commands.py
@@ -28,7 +28,8 @@ UCTERegionType: TypeAlias = Literal[
 CGMESRegionType: TypeAlias = Literal[
     "AL","AD","AM","AT","AZ","BY","BE","BA","BG","HR","CY","CZ","DK","EE","FI","FR",
     "GE","DE","GR","HU","IS","IE","IT","LV","LI","LT","LU","MT","MD","MC","ME","NL",
-    "MK","NO","PL","PT","RO","RU","SM","RS","SK","SI","ES","SE","CH","TR","UA","GB","VA"
+    "MK","NO","PL","PT","RO","RU","SM","RS","SK","SI","ES","SE","CH","TR","UA","GB","VA",
+    "HK"
     ]
 
 # fmt: on

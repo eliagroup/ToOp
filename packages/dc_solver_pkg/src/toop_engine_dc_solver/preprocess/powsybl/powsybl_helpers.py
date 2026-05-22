49e0cfd31157c98367a1f7379fab6f70da2f4acd
Leonard Hilfrich <Leonard.Hilfrich@50Hertz.com>
2026-05-22 10:57:06 +0200

    fix: linearity again
    Signed-off-by: Leonard Hilfrich <Leonard.Hilfrich@50Hertz.com>

--- packages/dc_solver_pkg/src/toop_engine_dc_solver/preprocess/powsybl/powsybl_helpers.py
+++ packages/dc_solver_pkg/src/toop_engine_dc_solver/preprocess/powsybl/powsybl_helpers.py
@@ -373,8 +373,7 @@ def get_linear_pst(net: Network, mode: Literal["ac", "dc"], tol: float = 1e-9) -
         pst_info = tap_steps.loc[pst_id]
         for col in lineal_col:
             pst_info_col = pst_info[col].values
-            diffs_from_start = pst_info_col - pst_info_col[0]
-            if np.any(np.abs(diffs_from_start) - tol > 0):
+            if not np.allclose(pst_info_col, pst_info_col[0], atol=tol):
                 trafo_linear_pst[pst_id] = False
                 break
 

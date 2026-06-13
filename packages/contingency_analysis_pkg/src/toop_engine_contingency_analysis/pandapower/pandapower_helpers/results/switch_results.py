f81fda3ca196dd51ad2f6705567391351b5e594d
siarhei <ser.matyushenok@gmail.com>
2026-06-13 12:04:02 +0200

    fix: fix tests

--- packages/contingency_analysis_pkg/src/toop_engine_contingency_analysis/pandapower/pandapower_helpers/results/switch_results.py
+++ packages/contingency_analysis_pkg/src/toop_engine_contingency_analysis/pandapower/pandapower_helpers/results/switch_results.py
@@ -571,7 +571,7 @@ def get_switch_mapped_elements(
     net : pp.pandapowerNet
         Pandapower network containing buses, switches, and branch elements.
     monitored_elements : pat.DataFrame[PandapowerMonitoredElementSchema]
-        Table of monitored elements. Only rows with ``kind == "switch"`` are used.
+        Table of monitored elements. Only rows with ``kind == "switch"`` or ``kind == "switch_relay"`` are used.
     side : Literal["bus", "element"]
         Defines from which side of the switch the traversal starts:
 
@@ -590,7 +590,7 @@ def get_switch_mapped_elements(
         - a branch-like element with a defined ``side``
         - a bus (with ``side = NaN``)
     """
-    monitored_switches = monitored_elements.query("kind == 'switch'")["table_id"].to_list()
+    monitored_switches = monitored_elements.query("kind in ('switch', 'switch_relay')")["table_id"].to_list()
 
     branch_map_df, bus_map_df = _get_switch_mapped_elements_by_origin_ids(net, monitored_switches, side)
 

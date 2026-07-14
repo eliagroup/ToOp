db0a9ca1cd09cabf9102955655664744dad0ad0f
Benjamin Petrick <170433522+BenjPetr@users.noreply.github.com>
2026-07-14 11:48:15 +0200

    test: fix rho detection test due to new pst that entered the grid
    Signed-off-by: Benjamin Petrick <170433522+BenjPetr@users.noreply.github.com>

--- packages/grid_helpers_pkg/tests/powsybl/test_powsybl_example_grids.py
+++ packages/grid_helpers_pkg/tests/powsybl/test_powsybl_example_grids.py
@@ -83,13 +83,17 @@ def test_create_complex_grid_battery_hvdc_svc_3w_trafo_nonlinear_psts_vary_rho()
     nonlinear_net = create_complex_grid_battery_hvdc_svc_3w_trafo(linear_pst=np.array([False, False, False]))
     nonlinear_steps = nonlinear_net.get_phase_tap_changer_steps(attributes=["rho"])
 
+    found_rho_values = 0
     for pst_id in nonlinear_steps.index.get_level_values("id").unique():
         rho_values = nonlinear_steps.loc[pst_id]["rho"].to_numpy(dtype=float)
-        assert not np.allclose(rho_values, rho_values[0])
+        if not np.allclose(rho_values, rho_values[0]):
+            found_rho_values += 1
+    assert found_rho_values >= 2, "Expected at least 2 phase tap changers with varying rho values with this configuration."
 
     linear_net = create_complex_grid_battery_hvdc_svc_3w_trafo(linear_pst=np.array([True, True, True]))
     linear_steps = linear_net.get_phase_tap_changer_steps(attributes=["rho"])
 
+    # Note, if rho changes, it is not linear anymore and therefore tests that expect linearity would fail.
     for pst_id in linear_steps.index.get_level_values("id").unique():
         rho_values = linear_steps.loc[pst_id]["rho"].to_numpy(dtype=float)
         assert np.allclose(rho_values, rho_values[0])
